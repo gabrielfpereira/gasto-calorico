b160a45db592f283e063fb5e2e7dcd0c7d7f46cf
gabrielfpereira <gabrielf.pereira@outlook.com>
2022-09-20 21:51:50 -0300

    Adicionado mais alimentos

--- components/refeicoes/caloriasAlimentos.js
+++ components/refeicoes/caloriasAlimentos.js
@@ -34,4 +34,114 @@ export const alimentos = [
         gramas: 100,
         calorias: 238
     },
+    {
+        nome: '2 fatias de pão integral',
+        gramas: 50,
+        calorias: 114
+    },
+    {
+        nome: 'Abacaxi',
+        gramas: 100,
+        calorias: 50
+    },
+    {
+        nome: 'Açaí',
+        gramas: 100,
+        calorias: 110
+    },
+    {
+        nome: 'Acem muido cozido',
+        gramas: 100,
+        calorias: 212
+    },
+    {
+        nome: 'Alface',
+        gramas: 100,
+        calorias: 15
+    },
+    {
+        nome: 'Banana nanica',
+        gramas: 100,
+        calorias: 87
+    },
+    {
+        nome: 'Banana prata',
+        gramas: 100,
+        calorias: 40
+    },
+    {
+        nome: 'Batata doce',
+        gramas: 100,
+        calorias: 86
+    },
+    {
+        nome: 'Bolacha de chocolate',
+        gramas: 60,
+        calorias: 211
+    },
+    {
+        nome: 'Calabresa',
+        gramas: 50,
+        calorias: 162
+    },
+    {
+        nome: 'Coxinha da asa',
+        gramas: 73,
+        calorias: 125
+    },
+    {
+        nome: 'Frango ao molhor de tomate',
+        gramas: 71,
+        calorias: 115
+    },
+    {
+        nome: 'Frango assado',
+        gramas: 100,
+        calorias: 260
+    },
+    {
+        nome: 'Maça',
+        gramas: 100,
+        calorias: 52
+    },
+    {
+        nome: 'Macarrão com carne muida',
+        gramas: 290,
+        calorias: 310
+    },
+    {
+        nome: 'Mamão',
+        gramas: 100,
+        calorias: 53
+    },
+    {
+        nome: 'Ovo frito na manteiga',
+        gramas: 50,
+        calorias: 110
+    },
+    {
+        nome: 'Pão de queijo',
+        gramas: 26,
+        calorias: 71
+    },
+    {
+        nome: 'Pão francês',
+        gramas: 100,
+        calorias: 310
+    },
+    {
+        nome: 'Pastel de carne',
+        gramas: 80,
+        calorias: 202
+    },
+    {
+        nome: 'Tapioca',
+        gramas: 100,
+        calorias: 130
+    },
+    {
+        nome: 'Torrada',
+        gramas: 100,
+        calorias: 313
+    },
 ]
